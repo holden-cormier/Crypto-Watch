ba2c5b6ea52407381d95933eb7bdcefef61af927
tej-sharma <trollgenstudios@gmail.com>
2021-11-30 10:51:35 -0500

    add data collection pipeline

--- app/src/main/java/com/example/cryptowatch/MainActivity.java
+++ app/src/main/java/com/example/cryptowatch/MainActivity.java
@@ -2,14 +2,43 @@ package com.example.cryptowatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final String LOG_TAG = "CRYPTO-WATCH";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FetchCryptoData fetchCryptoData = new FetchCryptoData(this);
+        fetchCryptoData.execute();
     }
 
+    private class FetchCryptoData extends AsyncTask<Void, Void, CryptoItem> {
+        Context context;
+
+        public FetchCryptoData(Context context) {
+            this.context = context;
+        }
+
+        protected CryptoItem doInBackground(Void... unused) {
+            CryptoItem result;
+            result = new CryptoItem("Ethereum", 4500f, "eth");
+            return result;
+        }
+
+        protected void onPostExecute(CryptoItem result) {
+            // Do something with the result.
+            handleCryptoDataResult(result);
+        }
+    }
+
+    private void handleCryptoDataResult(CryptoItem result) {
+        Log.d("CRYPTO-WATCH", result.name);
+    }
 }
