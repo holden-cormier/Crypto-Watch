3c0ac3b1ef54e21fe92634976c1117f23595dd9c
tej-sharma <trollgenstudios@gmail.com>
2021-12-02 10:55:42 -0500

    view update

--- app/src/main/java/com/example/cryptowatch/CryptoItem.java
+++ app/src/main/java/com/example/cryptowatch/CryptoItem.java
@@ -4,14 +4,17 @@ import android.media.Image;
 
 import java.util.LinkedList;
 
+// Class to store a crypto item's data
 public class CryptoItem {
     public String name;
     public float price;
     public String symbol;
+    public String imageURL;
 
-    public CryptoItem(String name, float price, String symbol) {
+    public CryptoItem(String name, float price, String symbol, String imageURL) {
         this.name = name;
         this.price = price;
         this.symbol = symbol;
+        this.imageURL = imageURL;
     }
 }
